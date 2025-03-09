ac317175b688fa8251606633ba0d1f440552abe8
Violacordis <ikemviolacordis@gmail.com>
2025-03-09 15:13:03 +0100

    initial migration

---
+++ prisma/migrations/20250309141224_create_models/migration.sql
@@ -0,0 +1,54 @@
+-- CreateEnum
+CREATE TYPE "TicketStatus" AS ENUM ('CheckedIn', 'Served');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" VARCHAR(75) NOT NULL,
+    "name" VARCHAR(75),
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "patients" (
+    "id" TEXT NOT NULL,
+    "email" VARCHAR(75) NOT NULL,
+    "name" VARCHAR(75) NOT NULL,
+    "regNum" VARCHAR(50) NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "patients_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tickets" (
+    "id" TEXT NOT NULL,
+    "ticketNumber" VARCHAR(50) NOT NULL,
+    "ticketDate" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "patientId" VARCHAR(50) NOT NULL,
+    "status" "TicketStatus" NOT NULL DEFAULT 'CheckedIn',
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "tickets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "patients_email_key" ON "patients"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "patients_regNum_key" ON "patients"("regNum");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tickets_ticketDate_ticketNumber_key" ON "tickets"("ticketDate", "ticketNumber");
+
+-- AddForeignKey
+ALTER TABLE "tickets" ADD CONSTRAINT "tickets_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
